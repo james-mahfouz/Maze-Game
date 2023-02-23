7a73069b877eddc835ccd73b69be73759f3fcb01
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-23 20:24:46 +0200

    Added the scoring system

--- game.js
+++ game.js
@@ -3,8 +3,10 @@ window.onload = function() {
     //creating variable and assigning elements by searching for IDs and class name
     var start = document.getElementById('start')
     var boundaries = document.getElementsByClassName('boundary')
-    var status =document.getElementsByClassName('example')[0]
+    var status = document.getElementById('status')
     var end = document.getElementById('end')
+    var score = document.getElementsByClassName('example')[0]
+    score.innerHTML = 0
 
     //when the mouse hover over the start dive the following apply
     start.addEventListener('mouseover', function(){
@@ -19,6 +21,7 @@ window.onload = function() {
         for (var i = 0; i < boundaries.length; i++) {         
             boundaries[i].addEventListener('mouseover', function() {
                 if (status.innerHTML=="Game Started"){
+                    score.innerHTML = parseInt(score.innerHTML) -10
                     console.log("lose")
                     for(var j = 0; j<boundaries.length; j++){
                         boundaries[j].classList.add('youlose')    
@@ -31,10 +34,10 @@ window.onload = function() {
         
         end.addEventListener('mouseover', function(){
             if (status.innerHTML == "Game Started"){
+                score.innerHTML = parseInt(score.innerHTML) + 5
                 console.log("win")
                 status.innerHTML = "You Win"
-            }  
-                     
+            }                       
         })
     }) 
 }
