3e889285cef46d2919cd635d9b95cad984459c08
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-23 15:27:28 +0200

    added an event listener to for when we click on start and hover over a boundary

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-let status = getElementbyId
+window.onload = function() {
+    var start = document.getElementById('start')
+    var status = document.getElementById('status')
+    var boundaries = document.getElementsByClassName('boundary')
+
+
+    start.addEventListener('click', function(){
+        console.log("Game has started")
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].addEventListener('mouseover', function() {
+              // Code to execute when the mouse hovers over the boundary element
+              console.log('Mouse is hovering over a boundary element!');
+            });
+          }
+    })
+  }
+  
