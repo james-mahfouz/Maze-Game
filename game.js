36cd321ce6942282ffe0a99176a98a925f3301f0
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-23 16:08:32 +0200

    The walls regain there original color when the mouse rehover over start

--- game.js
+++ game.js
@@ -6,14 +6,20 @@ window.onload = function() {
 
     start.addEventListener('click', function(){
         console.log("Game has started")
+
         for (var i = 0; i < boundaries.length; i++) {
             boundaries[i].addEventListener('mouseover', function() {
                 console.log('Mouse is hovering over a boundary element!');
                 for(var j = 0; j<boundaries.length; j++){
                     boundaries[j].classList.add('youlose')
                 }
-            });
-          }
+            })
+        }
+        start.addEventListener('mouseover', function(){
+            for(var j = 0; j<boundaries.length; j++){
+                boundaries[j].classList.remove('youlose')
+            }
+        })
     })
   }
   
